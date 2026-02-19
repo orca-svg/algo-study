69026e9f979190c49aa5ebe77a920f86a6f9f3fc
onff02 <165152345+onff02@users.noreply.github.com>
2026-02-19 17:14:44 +0900

    pro/lv.0/arraymultiply (#82)
    * [level 0] Title: 배열 두배 만들기, Time: 0.37 ms, Memory: 4.14 MB -BaekjoonHub
    
    * auto: merge solved problems into yetpi0413 folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ yetpi0413/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
+    
+    for(int& n : numbers) {
+        answer.push_back(n*2);
+    }
+    return answer;
+}
