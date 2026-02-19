191b69735e04ed0e17c969d1126b24f94611bc23
orca <orca@sparcs.org>
2026-02-19 15:35:06 +0900

    Boj/bronze/11654 (#78)
    * [Bronze V] Title: 아스키 코드, Time: 92 ms, Memory: 9344 KB -BaekjoonHub
    
    * auto: merge baekjoon files into j_orca folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ j_orca/백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+solution(input);
+
+function solution(input) {
+    const [target] = input;
+    const answer = target.charCodeAt(0);
+    console.log(answer);
+}
