7dcf1e1d0eb1f9592ca55d444186d07303328e50
onff02 <165152345+onff02@users.noreply.github.com>
2026-03-16 14:22:02 +0900

    pro/lv.0/removevowel (#379)
    * [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub
    
    * auto: merge solved problems into yetpi0413 folder
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

---
+++ yetpi0413/프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(auto& c : my_string){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')    continue;
+        answer += c;
+    }
+    return answer;
+}
